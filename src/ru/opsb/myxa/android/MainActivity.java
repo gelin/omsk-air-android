18fa12f034cf609bbe54ada6dc8840816a57fa18
Denis Nelubin <dnelubin@gmail.com>
2010-01-06 23:37:10 +0600

    Improved UI (a little). Added more resources.

--- src/ru/opsb/myxa/android/MainActivity.java
+++ src/ru/opsb/myxa/android/MainActivity.java
@@ -1,5 +1,8 @@
 package ru.opsb.myxa.android;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,30 +11,48 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	/**
-	 * 	Handler which handles temperature value.
-	 */
+    /**
+     * 	Handler which handles temperature value.
+     */
     final Handler handler = new Handler() {
         public void handleMessage(Message msg) {
-            float temp = msg.getData().getFloat("temperature");
-            TextView text = (TextView)findViewById(R.id.temperature);
-            text.setText("Температура в Омске: " + temp);
+            Float temp = msg.getData().getFloat(
+                    TemperatureGetter.TEMPERATURE);
+            TextView tempView = (TextView)findViewById(R.id.temp_value);
+            tempView.setText(formatTemperature(temp));
+            long lastModified = msg.getData().getLong(
+                    TemperatureGetter.LAST_MODIFIED);
+            TextView dateView = (TextView)findViewById(R.id.temp_date);
+            dateView.setText(formatDate(lastModified));
         }
     };	
-	
-	/** Called when the activity is first created. */
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
-    
+
     /**	Called when the activity becomes active. */
     @Override
     public void onResume() {
-    	super.onResume();
-    	TemperatureGetter getter = new TemperatureGetter(handler);
-    	getter.start();
+        super.onResume();
+        TemperatureGetter getter = new TemperatureGetter(handler);
+        getter.start();
     }
     
+    String formatDate(long timestamp) {
+        SimpleDateFormat format = new SimpleDateFormat(
+                getResources().getString(R.string.date_format));
+        return format.format(new Date(timestamp));
+    }
+    
+    String formatTemperature(Float temperature) {
+        if (temperature == null) {
+            return getResources().getString(R.string.no_temp);
+        }
+        return getResources().getString(R.string.temp_format, temperature);
+    }
+
 }
--- src/ru/opsb/myxa/android/TemperatureGetter.java
+++ src/ru/opsb/myxa/android/TemperatureGetter.java
@@ -14,73 +14,79 @@ import android.util.Log;
 
 public class TemperatureGetter extends Thread {
 
-	/**	URL of the temperature source */
-	static final String URL = "http://myxa.opsb.ru/files/weather.js";
-	
-	/** Regexp to parse JS */
-	static final Pattern JS_PATTERN = Pattern.compile("Therm\\s*=\\s*['\"](-?\\d+(\\.\\d+)?)['\"]");
-	/**	Group number with temperature result */
-	static final int JS_GROUP = 1;
-	
-	/**	Hander which receives the temperature */
-	Handler handler;
-	
-	/**
-	 * 	Constructs temperature getter for specified handler.
-	 */
-	public TemperatureGetter(Handler handler) {
-		this.handler = handler;
-	}
-	
-	/**
-	 * 	Runs the temperature getter.
-	 */
-	public void run() {
-		try {
-			float temp = getTemperature();
-			Message msg = handler.obtainMessage();
-			Bundle b = new Bundle();
-			b.putFloat("temperature", temp);
-			msg.setData(b);
-			handler.sendMessage(msg);
-		} catch (IOException e) {
-			Log.e(this.getClass().getName(), "Failed to get temperature", e);
-		}
-	}
-	
-	/**
-	 * 	Asks the temperature by the URL.
-	 * 	@throws IOException 
-	 */
-	static float getTemperature() throws IOException {
-		URL url = new URL(URL);
-		URLConnection connection = url.openConnection();
-		connection.connect();
-		byte[] buf = new byte[connection.getContentLength()];
-		int read = 0;
-		InputStream stream = connection.getInputStream();
-		do {
-			read += stream.read(buf);
-		} while (read < buf.length);
-		String js = new String(buf, "ISO-8859-1");
-		return parseTemperature(js);
-	}
-	
-	/**
-	 * 	Parses the temperature, encoded in JavaScript code.
-	 */
-	static float parseTemperature(String js) {
-		Matcher m = JS_PATTERN.matcher(js);
-		if (!m.find()) {
-			Log.w(TemperatureGetter.class.getName(), "Failed to parse: " + js);
-			return 0;
-		}
-		try {
-			return Float.parseFloat(m.group(JS_GROUP));
-		} catch (NumberFormatException e) {
-			Log.w(TemperatureGetter.class.getName(), "Failed to parse: " + js);
-			return 0;
-		}
-	}
-	
+    /** Result bundle key with the temperature value. */
+    public static final String TEMPERATURE = "temperature";
+    /** Result bundle key with the last modified value. */
+    public static final String LAST_MODIFIED = "last_modified";
+    
+    /**	URL of the temperature source */
+    static final String URL = "http://myxa.opsb.ru/files/weather.js";
+
+    /** Regexp to parse JS */
+    static final Pattern JS_PATTERN = Pattern.compile("Therm\\s*=\\s*['\"](-?\\d+(\\.\\d+)?)['\"]");
+    /**	Group number with temperature result */
+    static final int JS_GROUP = 1;
+
+    /**	Hander which receives the temperature */
+    Handler handler;
+
+    /**
+     * 	Constructs temperature getter for specified handler.
+     */
+    public TemperatureGetter(Handler handler) {
+        this.handler = handler;
+    }
+
+    /**
+     * 	Runs the temperature getter.
+     */
+    public void run() {
+        try {
+            Bundle result = getTemperatureBundle();
+            Message msg = handler.obtainMessage();
+            msg.setData(result);
+            handler.sendMessage(msg);
+        } catch (IOException e) {
+            Log.e(this.getClass().getName(), "Failed to get temperature", e);
+        }
+    }
+
+    /**
+     * 	Asks the temperature by the URL.
+     * 	@throws IOException 
+     */
+    static Bundle getTemperatureBundle() throws IOException {
+        Bundle result = new Bundle();
+        URL url = new URL(URL);
+        URLConnection connection = url.openConnection();
+        connection.connect();
+        result.putLong(LAST_MODIFIED, connection.getLastModified());
+        byte[] buf = new byte[connection.getContentLength()];
+        int read = 0;
+        InputStream stream = connection.getInputStream();
+        do {
+            read += stream.read(buf);
+        } while (read < buf.length);
+        String js = new String(buf, "ISO-8859-1");
+        result.putFloat(TEMPERATURE, parseTemperature(js));
+        return result;
+    }
+
+    /**
+     * 	Parses the temperature, encoded in JavaScript code.
+     */
+    static Float parseTemperature(String js) {
+        Matcher m = JS_PATTERN.matcher(js);
+        if (!m.find()) {
+            Log.w(TemperatureGetter.class.getName(), "Failed to parse: " + js);
+            return null;
+        }
+        try {
+            return Float.parseFloat(m.group(JS_GROUP));
+        } catch (NumberFormatException e) {
+            Log.w(TemperatureGetter.class.getName(), "Failed to parse: " + js);
+            return null;
+        }
+    }
+
 }
