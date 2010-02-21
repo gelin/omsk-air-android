13a4b1ab547581531554f06b6c75e41771318f33
Denis Nelubin <dnelubin@gmail.com>
2010-02-21 21:46:04 +0600

    Now changing of config echos immediately.

--- src/ru/opsb/myxa/android/Preferences.java
+++ src/ru/opsb/myxa/android/Preferences.java
@@ -1,19 +1,55 @@
 package ru.opsb.myxa.android;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 /**
  *  Activity to display application preferences.
  */
-public class Preferences extends PreferenceActivity {
+public class Preferences extends PreferenceActivity 
+        implements OnSharedPreferenceChangeListener {
 
+    /** Shared preferences which saves configuration options
+     *  displayed by this activity. */
+    SharedPreferences prefs;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+        
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (prefs == null) {
+            return;
+        }
+        prefs.registerOnSharedPreferenceChangeListener(this);
+    }
+    
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (prefs == null) {
+            return;
+        }
+        prefs.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onSharedPreferenceChanged(
+            SharedPreferences prefs, String name) {
+        Intent updateIntent = new Intent(UpdateService.ACTION_UPDATE_ALL);
+        updateIntent.setClass(this, UpdateService.class);
+        startService(updateIntent);
     }
 
 }
