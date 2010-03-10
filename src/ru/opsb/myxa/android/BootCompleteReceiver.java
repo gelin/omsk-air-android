cf1212d572458ea37dc485dc911b3f219efee82d
Denis Nelubin <dnelubin@gmail.com>
2010-03-10 18:18:52 +0600

    Added receiver on device boot which starts update.

---
+++ src/ru/opsb/myxa/android/BootCompleteReceiver.java
@@ -0,0 +1,18 @@
+package ru.opsb.myxa.android;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.BroadcastReceiver;
+
+/**
+ *  Broadcast receiver which receives event about boot complete.
+ *  Starts UpdateService.
+ */
+public class BootCompleteReceiver extends BroadcastReceiver
+        implements Constants {
+
+    public void onReceive (Context context, Intent intent) {
+        context.startService(UpdateService.UPDATE_ALL_INTENT);
+    }
+
+}
