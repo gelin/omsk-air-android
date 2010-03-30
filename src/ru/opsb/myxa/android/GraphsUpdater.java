0f9e726c1927c3e9145e069c6e8acecc70e03334
Denis Nelubin <dnelubin@gmail.com>
2010-03-30 22:26:20 +0700

    Experimenting with MediaStore. Images are saved but without albums :(

--- src/ru/opsb/myxa/android/GraphsUpdater.java
+++ src/ru/opsb/myxa/android/GraphsUpdater.java
@@ -4,7 +4,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -21,6 +25,7 @@ import android.util.Log;
 public class GraphsUpdater implements Runnable, Constants {
 
     static class GraphInfo {
+        public long id = -1;    //-1 means not existed
         public String url;
         public String name;
         public long lastModified;
@@ -61,7 +66,7 @@ public class GraphsUpdater implements Runnable, Constants {
     static final String[] LAST_MODIFIED_PROJECTION = {
         MediaStore.Images.ImageColumns._ID,
         MediaStore.Images.ImageColumns.DATE_TAKEN,
-        MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
+        MediaStore.Images.ImageColumns.DISPLAY_NAME,
     };
     /** Selection to select last modified date for the images */
     static final String LAST_MODIFIED_SELECTION = 
@@ -72,7 +77,9 @@ public class GraphsUpdater implements Runnable, Constants {
     /** Content resolver */
     ContentResolver contentResolver;
     /** Graphs information currently processed */
-    GraphInfo[] graphs;
+    List<GraphInfo> graphs;
+    /** Graphs information currently processed by the graph name */
+    Map<String, GraphInfo> graphsMap;
     /** Empty graph image */
     byte[] emptyGraph;
     
@@ -83,9 +90,12 @@ public class GraphsUpdater implements Runnable, Constants {
     public GraphsUpdater(Context context) {
         this.context = context;
         contentResolver = context.getContentResolver();
-        graphs = new GraphInfo[GRAPHS.length];
-        for (int i = 0; i < graphs.length; i++) {
-            graphs[i] = GRAPHS[i].copy();
+        graphs = new ArrayList<GraphInfo>();
+        graphsMap = new HashMap<String, GraphInfo>();
+        for (int i = 0; i < GRAPHS.length; i++) {
+            GraphInfo graphInfo = GRAPHS[i].copy();
+            graphs.add(graphInfo);
+            graphsMap.put(graphInfo.name, graphInfo);
         }
     }
     
@@ -93,8 +103,8 @@ public class GraphsUpdater implements Runnable, Constants {
      *  Updates all graphs.
      */
     public void run() {
-        getLastModified(graphs);
-        insertEmptyImages(graphs);
+        getLastModified();
+        insertEmptyImages();
     }
     
     /**
@@ -115,40 +125,46 @@ public class GraphsUpdater implements Runnable, Constants {
      *  Gets the previously saved last modified time for the graph.
      *  @param  graphInfos   lastModified property of this objects is updated
      */
-    void getLastModified(GraphInfo[] graphInfos) {
+    void getLastModified() {
         Cursor cursor = contentResolver.query(IMAGES_URI,
                 LAST_MODIFIED_PROJECTION, LAST_MODIFIED_SELECTION, null, null);
         
         if (cursor != null && cursor.moveToFirst()) {
 
-            String name; 
-            long lastModified; 
+            int idColumn = cursor.getColumnIndex(
+                    MediaStore.Images.ImageColumns._ID);
             int nameColumn = cursor.getColumnIndex(
-                    MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME); 
+                    MediaStore.Images.ImageColumns.DISPLAY_NAME);
             int lastModifiedColumn = cursor.getColumnIndex(
                     MediaStore.Images.ImageColumns.DATE_TAKEN);
         
             do {
-                name = cursor.getString(nameColumn);
-                lastModified = cursor.getLong(lastModifiedColumn);
-
+                String name = cursor.getString(nameColumn);
+                GraphInfo graphInfo = graphsMap.get(name);
+                if (graphInfo == null) {
+                    continue;
+                }
+                graphInfo.id = cursor.getLong(idColumn);
+                graphInfo.lastModified = cursor.getLong(lastModifiedColumn);
+                Log.d(TAG, "found image " + name + " id = " + graphInfo.id);
             } while (cursor.moveToNext());
 
             cursor.close();
-
         }
-
     }
     
     /**
      *  Inserts empty images if the lastModified is empty.
      *  @param  graphInfo   lastModified property of this object is updated
      */
-    void insertEmptyImages(GraphInfo[] graphInfos) {
-        for (GraphInfo graphInfo : graphInfos) {
-            if (graphInfo.lastModified > 0) {
+    void insertEmptyImages() {
+        for (GraphInfo graphInfo : graphs) {
+            if (graphInfo.id >= 0) {
                 continue;
             }
+            
+            Log.d(TAG, "inserting " + graphInfo.name);
+            
             ContentValues values = new ContentValues();
             values.put(MediaStore.Images.ImageColumns.DISPLAY_NAME, 
                     graphInfo.name);
@@ -160,6 +176,7 @@ public class GraphsUpdater implements Runnable, Constants {
                     BUCKET_ID);
             
             Uri uri = contentResolver.insert(IMAGES_URI, values);
+            Log.d(TAG, "inserted " + uri);
             
             if (emptyGraph == null) {
                 InputStream in = context.getResources().openRawResource(R.drawable.empty_graph);
@@ -168,6 +185,7 @@ public class GraphsUpdater implements Runnable, Constants {
                     int b = in.read();
                     while (b >= 0) {
                         out.write(b);
+                        b = in.read();
                     }
                 } catch (IOException e) {
                     Log.e(TAG, "failed to load empty graph drawable from resources", e);
@@ -184,28 +202,6 @@ public class GraphsUpdater implements Runnable, Constants {
                 Log.e(TAG, "exception while writing image", e); 
             }
         }
-        
-        Cursor cursor = contentResolver.query(IMAGES_URI,
-                LAST_MODIFIED_PROJECTION, LAST_MODIFIED_SELECTION, null, null);
-        
-        if (cursor != null && cursor.moveToFirst()) {
-
-            String name; 
-            long lastModified; 
-            int nameColumn = cursor.getColumnIndex(
-                    MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME); 
-            int lastModifiedColumn = cursor.getColumnIndex(
-                    MediaStore.Images.ImageColumns.DATE_TAKEN);
-        
-            do {
-                name = cursor.getString(nameColumn);
-                lastModified = cursor.getLong(lastModifiedColumn);
-
-            } while (cursor.moveToNext());
-
-        }
-        
-        cursor.close();
     }
     
     /**
